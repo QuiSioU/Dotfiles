393ca6997e68e4dfe13629dedb2ebe44bfb70470
= <=>
2026-01-17 20:18:54 +0100

    Added bar, and a clock to the bar

--- eww/src/clock.c
+++ eww/src/clock.c
@@ -4,19 +4,19 @@
 int main() {
     time_t      unix_time = 0;
     struct tm   *info;
-    char        day[3], weekday[4], month_num[3], month[4], year[5], time_str[6]; // Include for \0
+    char        day[3], weekday[4], month_num[3], month[4], year[5], time_str[9]; // Include for \0
 
     time(&unix_time);
     info = localtime(&unix_time);
 
     if (!info) return 1;
     
-    strftime(day,       3, "%d",    info);
-    strftime(weekday,   4, "%a",    info);
-    strftime(month_num, 3, "%m",    info);
-    strftime(month,     4, "%b",    info);
-    strftime(year,      5, "%Y",    info);
-    strftime(time_str,  6, "%H:%M", info);
+    strftime(day,       3, "%d",        info);
+    strftime(weekday,   4, "%a",        info);
+    strftime(month_num, 3, "%m",        info);
+    strftime(month,     4, "%b",        info);
+    strftime(year,      5, "%Y",        info);
+    strftime(time_str,  9, "%H:%M:%S",  info);
 
     printf(
         "{\"day\": \"%s\", \"weekday\": \"%s\", \"month_num\": \"%s\", \"month\": \"%s\", \"year\": \"%s\", \"time\": \"%s\"}\n",
