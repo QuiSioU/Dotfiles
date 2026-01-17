1b0e3e23555086c55b8faac6646f17530563d8ca
= <=>
2026-01-18 00:07:39 +0100

    Improved battery source code

--- eww/src/battery.c
+++ eww/src/battery.c
@@ -4,25 +4,22 @@
 int main() {
     int capacity = 0;
     char status[32];
-    const char json_params[] = "{\"capacity\": %d, \"status\": \"%s\"}\n";
     
     FILE *f_cap = fopen("/sys/class/power_supply/BAT1/capacity", "r");
     FILE *f_stat = fopen("/sys/class/power_supply/BAT1/status", "r");
 
-    if (!f_cap || !f_stat) { // Battery missing or some sht
-        printf(json_params, 0, "Unknown");
-        if (f_cap) fclose(f_cap);
-        if (f_stat) fclose(f_stat);
-        return 1;
+    if (f_cap) {
+        fscanf(f_cap, "%d", &capacity);
+        fclose(f_cap);
     }
 
-    if (fscanf(f_cap, "%d", &capacity) != 1) capacity = 0;
-    if (fscanf(f_stat, "%[^\n]", status) != 1) strcpy(status, "Unknown");
+    if (f_stat) {
+        if (fgets(status, sizeof(status), f_stat)) {
+            status[strcspn(status, "\n")] = 0;
+        }
+        fclose(f_stat);
+    }
 
-    fclose(f_cap);
-    fclose(f_stat);
-
-    printf(json_params, capacity, status);
-
+    printf("{\"capacity\": %d, \"status\": \"%s\"}\n", capacity, status);
     return 0;
 }
