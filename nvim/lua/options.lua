b1ceb365489c0ca29b8a85c1a3a4e4c314ccc464
QuiSioU <marco.casteleiro@gmail.com>
2026-04-19 22:32:50 +0200

    Nvim 4space TABS and UNIX line end (lf)

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -9,16 +9,16 @@ opt.relativenumber = true
 opt.numberwidth    = 5
 
 -- Tabs / indentation
-opt.expandtab   = false
+opt.expandtab   = true
 opt.tabstop     = 4
-opt.softtabstop = 0
+opt.softtabstop = 4
 opt.shiftwidth  = 4
 opt.autoindent  = true
 opt.smarttab    = true
 
 -- Indentation markers
 opt.list      = true
-opt.listchars = { tab = "¦ " }
+opt.listchars = { leadmultispace = "¦   ", trail = "·" }
 
 -- Folding
 opt.foldmethod = "indent"
@@ -45,3 +45,13 @@ opt.swapfile = false
 vim.api.nvim_set_hl(0, "CursorLine",   { bg = "#3A3F58", cterm = {} })
 vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FFD700", cterm = {} })
 vim.api.nvim_set_hl(0, "LineNr",       { fg = "#33384A", cterm = {} })
+
+-- Remove trailing whitespace on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function()
+        local pos = vim.api.nvim_win_get_cursor(0)
+        vim.cmd([[%s/\s\+$//e]])
+        vim.api.nvim_win_set_cursor(0, pos)
+    end,
+})
