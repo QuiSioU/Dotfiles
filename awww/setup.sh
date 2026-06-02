4eaccf6dfd745ed206089fac57bd8f397ac9da47
QuiSioU <marco.casteleiro@gmail.com>
2026-06-02 10:27:37 +0200

    Fixed awww's setup.sh regarding cached file

--- awww/setup.sh
+++ awww/setup.sh
@@ -9,11 +9,22 @@ echo ""
 
 echo "Setting default wallpaper if cache is missing..."
 
-if [ -f "$HOME/.cache/awww/0.12.0/eDP-1" ]; then
-    echo "    skipped    $HOME/.cache/awww/0.12.0/eDP-1:  cached wallpaper already exists"
+AWWW_VERSION=$(awww --version | awk '{print $2}')
+
+if [ -z "$AWWW_VERSION" ]; then
+    echo "Error: Could not detect awww version."
+    exit 1
+fi
+
+# This only works for 1 monitor
+
+CACHE_FILE="$HOME/.cache/awww/$AWWW_VERSION/eDP-1"
+
+if [ -f "$CACHE_FILE" ]; then
+    echo "    skipped    $CACHE_FILE:  cached wallpaper already exists"
 else
     awww img "$HOME/.config/awww/default/Leshy.jpg" --transition-type center
-    echo "    created    $HOME/.cache/awww/0.12.0/eDP-1"
+    echo "    created    $CACHE_FILE"
 fi
 
 echo "╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌"
