70d2078c2487ed1dccc7e4b1cdb107291869e640
QuiSioU <marco.casteleiro@gmail.com>
2026-06-25 13:50:49 +0200

    Reverted most env vars changes

--- hypr/default/env.lua
+++ hypr/default/env.lua
@@ -3,6 +3,10 @@
 
 ----- ENVIRONMENT VARIABLES -------------------------------------------
 
+hl.env("MOZ_ENABLE_WAYLAND", 1)
+hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
+hl.env("XCURSOR_THEME", "Adwaita")
+hl.env("XCURSOR_SIZE", 24)
 hl.env("HYPRCURSOR_SIZE", 24)
 hl.env("HYPRSHOT_DIR", os.getenv("HOME") .. "/Pictures/Screenshots")
 
