b18fdb5fedf9071b0915935e4310422d3d9c138a
QuiSioU <marco.casteleiro@gmail.com>
2026-06-29 23:29:01 +0200

    Added default quick apps list generation

--- quickshell/setup.sh
+++ quickshell/setup.sh
@@ -50,4 +50,61 @@ cmake --build .build --parallel
 
 echo "╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌"
 
+echo "Creating default quick apps list..."
+
+userDir="widgets/user"
+
+if [ -L "$userDir" ]; then
+    echo "    skipped    $userDir: file already exists (symlink)"
+elif [ -e "$userDir" ]; then
+    echo "    skipped    $userDir: file already exists (not symlink)"
+else
+    mkdir -p "$userDir"
+    echo "    created    $userDir"
+fi
+
+quickAppsListfile="$userDir/QuickAppsList.qml"
+
+if [ -L "$quickAppsListfile" ]; then
+    echo "    skipped    $quickAppsListfile: file already exists (symlink)"
+elif [ -e "$quickAppsListfile" ]; then
+    echo "    skipped    $quickAppsListfile: file already exists (not symlink)"
+else
+    cat > $quickAppsListfile <<EOF
+// quickshell/$quickAppsListfile
+
+
+pragma Singleton
+import Quickshell
+
+Singleton {
+    // Add here your favourite apps (by their .desktop filename) to add them to quick access
+    readonly property list<string> apps: [
+        "codium",
+        "firefox",
+        "steam"
+    ]
+}
+EOF
+    echo "    created    $quickAppsListfile"
+fi
+
+qumldirfile="$userDir/qmldir"
+
+if [ -L "$qumldirfile" ]; then
+    echo "    skipped    $qumldirfile: file already exists (symlink)"
+elif [ -e "$qumldirfile" ]; then
+    echo "    skipped    $qumldirfile: file already exists (not symlink)"
+else
+    cat > $qumldirfile <<EOF
+# quickshell/$qumldirfile
+
+
+singleton QuickAppsList 1.0 QuickAppsList.qml
+EOF
+    echo "    created    $qumldirfile"
+fi
+
+echo "╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌"
+
 echo "Quickshell configured successfully!"
