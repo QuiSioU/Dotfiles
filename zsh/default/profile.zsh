73657b3924e680425dbbf697cdc9ea3e62e9bb88
quisiou <marco.casteleiro@gmail.com>
2026-08-20 12:19:39 +0100

    Fixed desktop portal problem (use hyprland with uwsm now)

--- zsh/default/profile.zsh
+++ zsh/default/profile.zsh
@@ -5,5 +5,5 @@
 # Start Hyprland Automatically on TTY1 only, so if something breaks,
 # you're still able to log in without hyprland on another TTY
 if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
-	exec start-hyprland
+	exec uwsm start default
 fi
